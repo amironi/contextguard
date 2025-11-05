b43603408a05ecf4258610006294abd2bbe6d488
Amir Mironi <am5050@gmail.com>
2025-11-05 13:55:51 +0200

    a

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-// Core types
 export * from "./types";
 
-// Policy
 export { createPolicyChecker, PolicyChecker, DEFAULT_POLICY } from "./policy";
-
-// Logger
 export { createLogger, Logger } from "./logger";
-
-// Agent
 export { createAgent, Agent } from "./agent";
-
-// CLI
 export { main } from "./cli";
